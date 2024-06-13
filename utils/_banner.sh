01e5b9ea568724992f5100df339b57097c9978e4
tonbts <30326287+tonbts@users.noreply.github.com>
2024-06-13 15:18:20 -0300

    first commit

---
+++ utils/_banner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+printf "${CYAN_LIGHT}";
+
+
+printf "TonChat";
+
+
+  printf "\n"
+}
